12e1de44dbe2a0c8daf3e1f4a8909ed9e5dafa6e
Jawahir Al Faqih <fmjawahir@gmail.com>
2023-10-19 01:43:25 +0700

    Final Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+![Logo](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi3fWA7QzBTo9ej0PxWunHfjyKTg5C5EwrLS-ulPCj8ZCeklEwr-8bqY3knBNu31RFtoR8OtrwJiJKc48dWggi-fbDmKasY4ZqHkFOY9TC_KdFIZGNy-PRxsvspjouWQMh6rZr5_32aK3niKcw7NglfV7KyjaJE2uqa1mx_jBFlJYnvORGb9slMZpDEF4_e/s515/bakeo-logo.jpg)Bake'O is a cake recipe website. Made with Vite, Bootstrap 5, Saas, and Javascript.
+### How to Run
+
+1. Clone this repository
+```bash
+  git clone https://github.com/fmjawahir/bakeo-cakerecipes-bootstrap5
+```
+
+2. Run it locally
+```sh
+npm run start
+```
+
+3. Open `http://localhost:yourport` in your browser
+
+## License
+
+This repository is under [MIT License](./LICENSE).
--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,34 @@ import "../scss/styles.scss";
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+// challangeForm
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("challangeForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Lakukan permintaan POST ke skrip Google Apps Script
+    fetch(
+      "https://script.google.com/macros/s/AKfycby3hIYlD3v2pZgMOB5cLi7gfR1atNruCovn9rJ5RxVG8gSgJCqrSP_oK4-VDIxBxES-/exec",
+      {
+        method: "POST",
+        body: new FormData(form),
+      }
+    )
+      .then(function (response) {
+        if (response.ok) {
+          // Form berhasil dikirim
+          alert("Formulir berhasil dikirim.");
+          location.reload(); // Memuat ulang halaman
+        } else {
+          // Gagal mengirim
+          alert("Gagal mengirim formulir.");
+        }
+      })
+      .catch(function (error) {
+        console.error("Terjadi kesalahan:", error);
+      });
+  });
+});
